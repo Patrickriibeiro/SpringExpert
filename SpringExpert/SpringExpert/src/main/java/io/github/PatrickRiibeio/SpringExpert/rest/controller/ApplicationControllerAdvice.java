53eeb69b17488d5217619e666a6c53b683f66363
Patrick Ribeiro Oliveira <patrick-riibeiro@hotmail.com>
2022-11-22 11:47:54 -0300

    Update ApplicationControllerAdvice.java

--- SpringExpert/SpringExpert/src/main/java/io/github/PatrickRiibeio/SpringExpert/rest/controller/ApplicationControllerAdvice.java
+++ SpringExpert/SpringExpert/src/main/java/io/github/PatrickRiibeio/SpringExpert/rest/controller/ApplicationControllerAdvice.java
@@ -1,6 +1,8 @@
 package io.github.PatrickRiibeio.SpringExpert.rest.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import io.github.PatrickRiibeio.SpringExpert.exception.RegraDeNegocioException;
@@ -11,6 +13,7 @@ import io.github.PatrickRiibeio.SpringExpert.utils.ApiErrors;
 public class ApplicationControllerAdvice {
 
 	 @ExceptionHandler(RegraDeNegocioException.class)
+	 @ResponseStatus(HttpStatus.BAD_REQUEST)
      public ApiErrors handleRegraNegocioException(RegraDeNegocioException ex) {
     	 String mensagemErro = ex.getMessage();
     	 return new ApiErrors(mensagemErro);
