092561cf159c13bf86b442c400c1c1c1793ca6e7
Patrick Ribeiro Oliveira <patrick-riibeiro@hotmail.com>
2022-12-17 12:01:05 -0300

    swagger

--- SpringExpert/SpringExpert/src/main/java/io/github/PatrickRiibeio/SpringExpert/config/SwaggerConfig.java
+++ SpringExpert/SpringExpert/src/main/java/io/github/PatrickRiibeio/SpringExpert/config/SwaggerConfig.java
@@ -1,5 +1,9 @@
 package io.github.PatrickRiibeio.SpringExpert.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,8 +11,12 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -26,6 +34,8 @@ public class SwaggerConfig {
 				.apis(RequestHandlerSelectors.basePackage("io.github.PatrickRiibeio.SpringExpert.rest.controller"))
 				.paths(PathSelectors.any())
 				.build()
+				.securitySchemes(Arrays.asList(this.apiKey()))
+				.securityContexts(Arrays.asList(this.securityContext()))
 				.apiInfo(this.apiInfo());
 		
 	}
@@ -40,5 +50,26 @@ public class SwaggerConfig {
 	
 	private Contact contact() {
 		return new Contact("Patrick Ribeiro Oliveira", "https://github.com/Patrickriibeiro", "patrickribeiro1198@gmail.com");
+	}
+	
+	public ApiKey apiKey() {
+		return new ApiKey("JWT","Authorization","header");
+	}
+	
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+				.securityReferences(this.defaulAuth())
+				.forPaths(PathSelectors.any())
+				.build();
+	}
+	
+	private List<SecurityReference> defaulAuth(){
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope [] scopes = new AuthorizationScope[1];
+		scopes [0] = authorizationScope;
+		SecurityReference reference = new SecurityReference("JWT", scopes);
+		List<SecurityReference> auths = new ArrayList<>();
+		auths.add(reference);
+		return auths;
 	}
 }
--- SpringExpert/SpringExpert/src/main/java/io/github/PatrickRiibeio/SpringExpert/rest/controller/ClienteController.java
+++ SpringExpert/SpringExpert/src/main/java/io/github/PatrickRiibeio/SpringExpert/rest/controller/ClienteController.java
@@ -20,9 +20,16 @@ import org.springframework.web.server.ResponseStatusException;
 
 import io.github.PatrickRiibeio.SpringExpert.domain.Entity.Cliente;
 import io.github.PatrickRiibeio.SpringExpert.domain.repository.ClientesRepositorySpringDataJpa;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api/clientes")
+//@ApiIgnore
+@Api("Api Clientes")
 public class ClienteController {
 
 	private ClientesRepositorySpringDataJpa repository;
@@ -32,13 +39,23 @@ public class ClienteController {
 	}
 
 	@GetMapping("{id}")
-	public Cliente getClienteById(@PathVariable Integer id) {
+	@ApiOperation("Obter detalhes do um cliente")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Cliente encontrado"),
+		@ApiResponse(code = 400, message = "Cliente não encotrado para o ID informado.")
+	})
+	public Cliente getClienteById(@PathVariable @ApiParam("Id do Cliente")  Integer id) {
 		return repository.findById(id)
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
 	}
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
+	@ApiOperation("Salva um novo cliente")
+	@ApiResponses({
+		@ApiResponse(code = 201, message = "Cliente salvo com sucesso"),
+		@ApiResponse(code = 400, message = "Cliente não encotrado para o ID informado.")
+	})
 	public Cliente save(@RequestBody @Valid Cliente cliente) {
 		return repository.save(cliente);
 	}
